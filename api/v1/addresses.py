ab6525085f8001319eb9fc051fdd7b1b429d4c61
RubenMarVil <ruben.marquez@alu.uclm.es>
2019-11-27 18:46:43 +0100

    Added the Address resource

--- api/v1/addresses.py
+++ api/v1/addresses.py
@@ -3,8 +3,10 @@
 from flask import Blueprint, jsonify, abort, make_response, request
 
 class Address:
-    def __init__(self, ID, type_address, name, number, stair, floor):
+    def __init__(self, ID, country, city, type_address, name, number, stair, floor):
         self.ID = ID
+        self.country = country
+        self.city = city
         self.type_address = type_address
         self.name = name
         self.number = number
@@ -13,7 +15,9 @@ class Address:
 
 ##############################################################################
 # DATA
-addresses_list = []
+addresses_list = [Address(1, 'Spain', 'Madrid', 'Street', 'Pablo Picasso', 8, 2, 4),
+Address(2, 'United Kingdom', 'London', 'Avenue', '23', 4, None, None),
+Address(3, 'Spain', 'Ciudad Real', 'Street', 'San Anton', 6, 0, 0)]
 ##############################################################################
 
 addresses_api = Blueprint('addresses_api', __name__)
@@ -34,15 +38,23 @@ def getOneAddress(address_id):
 # Create one address
 @addresses_api.route('/v1/addresses/', methods=['POST'])
 def createAddress():
-    if not request.json or not 'ID' in request.json:
-        abort(404)
-    ID = request.json.get('ID')
+    if not request.json or not 'country' in request.json:
+        abort(400)
+    ID = addresses_list[-1].ID + 1
+    country = request.json.get('country')
+    city = request.json.get('city')
     type_address = request.json.get('type_address')
     name = request.json.get('name')
     number = request.json.get('number')
-    stair = request.json.get('stair')
-    floor = request.json.get('floor')
-    new_address = Address(ID, type_address, name, number, stair, floor)
+    if 'stair' in request.json:
+        stair = request.json.get('stair')
+    else:
+        stair = None
+    if 'floor' in request.json:
+        floor = request.json.get('floor')
+    else:
+        floor = None
+    new_address = Address(ID, country, city, type_address, name, number, stair, floor)
     addresses_list.append(new_address)
     return jsonify({'ADDRESS-CREATED': new_address}), 201
 
@@ -52,6 +64,10 @@ def editAddress(address_id):
     if request.json:
         for address in addresses_list:
             if address.ID == address_id:
+                if 'country' in request.json:
+                    address.country = request.json.get('country')
+                if 'city' in request.json:
+                    address.city = request.json.get('city')
                 if 'type_address' in request.json:
                     address.type_address = request.json.get('type_address')
                 if 'name' in request.json:
@@ -64,6 +80,7 @@ def editAddress(address_id):
                     address.floor = request.json.get('floor')
                 return jsonify({'ADDRESS-EDITED': address}), 201
         abort(404)
+    abort(400)
 
 # Delete one address
 @addresses_api.route('/v1/addresses/<int:address_id>/', methods=['DELETE'])
@@ -72,8 +89,14 @@ def deleteAddress(address_id):
         if address.ID == address_id:
             addresses_list.remove(address)
             return jsonify({'ADDRESSES': addresses_list}), 200
+    abort(404)
 
 # 404 errors handle
 @addresses_api.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'ERROR': 'Not found'}), 404)
+
+# 400 errors handle
+@addresses_api.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'ERROR': 'Bad Request'}),400)
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from flask import Flask
 
 from api.v1.addresses import addresses_api, Address
+'''
 from api.v1.orders import orders_api, Order
 from api.v1.products import products_api, Product
 from api.v1.users import users_api, User
+'''
 
 from flask.json import JSONEncoder
 
 class MyJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Address):
-            return {
+            address = {
                 'ID': obj.ID,
+                'country': obj.country,
+                'city': obj.city,
                 'type_address': obj.type_address,
                 'name': obj.name,
-                'number': obj.number,
-                'stair': obj.stair,
-                'floor': obj.floor
+                'number': obj.number
             }
+            if obj.floor is not None:
+                address['floor'] = obj.floor
+            if obj.stair is not None:
+                address['stair'] = obj.stair
+            return address
+        '''
         if isinstance(obj, Order):
             return {
                 'ID': obj.ID,
@@ -44,6 +52,7 @@ class MyJSONEncoder(JSONEncoder):
                 'addresses': obj.addresses
                 'orders': obj.orders
             }
+        '''
         return super(MyJSONEncoder, self).default(obj)
 
 app = Flask(__name__)
@@ -51,9 +60,11 @@ app.json_encoder = MyJSONEncoder
 
 # Blueprint
 app.register_blueprint(addresses_api)
+'''
 app.register_blueprint(orders_api)
 app.register_blueprint(products_api)
 app.register_blueprint(users_api)
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
